770940ff90db9e4c35da2e12e1492ade4a41767b
Guichaguri <guichaguri@gmail.com>
2017-07-17 19:16:20 -0300

    Updated the JS bindings

--- index.android.js
+++ index.android.js
@@ -1,7 +1,4 @@
-const ReactNative = require('react-native');
-const TrackPlayer = ReactNative.NativeModules.TrackPlayerModule;
+// For documentation, check our wiki
+// https://github.com/Guichaguri/react-native-track-player/wiki
 
-module.exports = TrackPlayer;
-
-// TODO make video work
-//module.exports.Video = require('./Video.js');
+export * from './lib/index.js';
