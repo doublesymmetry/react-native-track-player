752ae898ebe39956b2f4aeb75a1b7302ea26463c
anderslemke <mail@anderslemke.dk>
2017-09-20 15:42:19 +0200

    [ios] Fix playing local files

--- ios/RNTrackPlayer/Models/MediaURL.swift
+++ ios/RNTrackPlayer/Models/MediaURL.swift
@@ -23,8 +23,8 @@ struct MediaURL {
             let encodedURI = uri.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
             value = URL(string: encodedURI)!
         } else {
-            isLocal = false
             let url = object as! String
+            isLocal = url.hasPrefix("file://")
             let encodedURI = url.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
             value = URL(string: encodedURI)!
         }
