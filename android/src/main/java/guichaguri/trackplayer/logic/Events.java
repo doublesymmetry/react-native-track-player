8483402aa63b12e92811a1a544fe9ac4e759eebd
Guichaguri <guichaguri@gmail.com>
2017-09-29 19:26:35 -0300

    Changed media controls event names

--- android/src/main/java/guichaguri/trackplayer/logic/Events.java
+++ android/src/main/java/guichaguri/trackplayer/logic/Events.java
@@ -11,17 +11,17 @@ import guichaguri.trackplayer.logic.services.PlayerTask;
 public class Events {
 
     // Media Control Events
-    public static final String BUTTON_PLAY = "play";
-    public static final String BUTTON_PLAY_FROM_ID = "playFromId";
-    public static final String BUTTON_PLAY_FROM_SEARCH = "playFromSearch";
-    public static final String BUTTON_PAUSE = "pause";
-    public static final String BUTTON_STOP = "stop";
-    public static final String BUTTON_SKIP = "skip";
-    public static final String BUTTON_SKIP_NEXT = "skipToNext";
-    public static final String BUTTON_SKIP_PREVIOUS = "skipToPrevious";
-    public static final String BUTTON_SEEK_TO = "seekTo";
-    public static final String BUTTON_SET_RATING = "setRating";
-    public static final String BUTTON_DUCK = "duck";
+    public static final String BUTTON_PLAY = "remote-play";
+    public static final String BUTTON_PLAY_FROM_ID = "remote-play-id";
+    public static final String BUTTON_PLAY_FROM_SEARCH = "remote-play-search";
+    public static final String BUTTON_PAUSE = "remote-pause";
+    public static final String BUTTON_STOP = "remote-stop";
+    public static final String BUTTON_SKIP = "remote-skip";
+    public static final String BUTTON_SKIP_NEXT = "remote-next";
+    public static final String BUTTON_SKIP_PREVIOUS = "remote-previous";
+    public static final String BUTTON_SEEK_TO = "remote-seek";
+    public static final String BUTTON_SET_RATING = "remote-set-rating";
+    public static final String BUTTON_DUCK = "remote-duck";
 
     // Playback Events
     public static final String PLAYBACK_STATE = "playback-state";
--- windows/RNTrackPlayer/Logic/Events.cs
+++ windows/RNTrackPlayer/Logic/Events.cs
@@ -8,12 +8,12 @@ namespace TrackPlayer.Logic {
 
     enum Events {
 
-        ButtonPlay = "play",
-        ButtonPause = "pause",
-        ButtonStop = "stop",
-        ButtonSkipNext = "skipToNext",
-        ButtonSkipPrevious = "skipToPrevious",
-        ButtonSeekTo = "seekTo",
+        ButtonPlay = "remote-play",
+        ButtonPause = "remote-pause",
+        ButtonStop = "remote-stop",
+        ButtonSkipNext = "remote-next",
+        ButtonSkipPrevious = "remote-previous",
+        ButtonSeekTo = "remote-seek",
 
         PlaybackState = "playback-state",
         PlaybackTrackChanged = "playback-track-changed",
