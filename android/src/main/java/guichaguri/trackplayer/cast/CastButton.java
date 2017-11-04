8111ac027e9d5f1c62d12619d48bd922a8105c68
Guichaguri <guichaguri@gmail.com>
2017-11-04 01:36:17 -0200

    Fixed CastButton not showing up. Closes #58

--- android/src/main/java/guichaguri/trackplayer/cast/CastButton.java
+++ android/src/main/java/guichaguri/trackplayer/cast/CastButton.java
@@ -6,6 +6,9 @@ import android.app.FragmentManager;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.PorterDuff.Mode;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.MediaRouteButton;
@@ -26,8 +29,33 @@ public class CastButton extends MediaRouteButton {
     private final String CHOOSER_FRAGMENT_TAG = "trackplayer.cast.chooser";
     private final String CONTROLLER_FRAGMENT_TAG = "trackplayer.cast.controller";
 
+    private Drawable remoteIndicator;
+    private int color = Color.TRANSPARENT;
+
     public CastButton(Context context) {
         super(context);
+    }
+
+    private void updateColor() {
+        if(remoteIndicator != null) {
+            if(color != Color.TRANSPARENT) {
+                remoteIndicator.setColorFilter(color, Mode.SRC_IN);
+            } else {
+                remoteIndicator.setColorFilter(null);
+            }
+        }
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+        updateColor();
+    }
+
+    @Override
+    public void setRemoteIndicatorDrawable(Drawable d) {
+        this.remoteIndicator = d;
+        updateColor();
+        super.setRemoteIndicatorDrawable(d);
     }
 
     public boolean showDialog() {
--- android/src/main/java/guichaguri/trackplayer/cast/CastButtonManager.java
+++ android/src/main/java/guichaguri/trackplayer/cast/CastButtonManager.java
@@ -1,9 +1,21 @@
 package guichaguri.trackplayer.cast;
 
+import android.app.MediaRouteButton;
+import android.graphics.Color;
+import android.view.View;
+import android.view.View.MeasureSpec;
+import android.view.ViewGroup.LayoutParams;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.common.MapBuilder;
+import com.facebook.react.uimanager.LayoutShadowNode;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.ViewProps;
+import com.facebook.react.uimanager.annotations.ReactProp;
+import com.facebook.yoga.YogaMeasureFunction;
+import com.facebook.yoga.YogaMeasureMode;
+import com.facebook.yoga.YogaMeasureOutput;
+import com.facebook.yoga.YogaNode;
 import com.google.android.gms.cast.framework.CastButtonFactory;
 import java.util.Map;
 import javax.annotation.Nullable;
@@ -28,9 +40,24 @@ public class CastButtonManager extends SimpleViewManager<CastButton> {
     }
 
     @Override
-    public void receiveCommand(CastButton root, int commandId, @Nullable ReadableArray args) {
+    public LayoutShadowNode createShadowNodeInstance() {
+        return new CastButtonShadowNode();
+    }
+
+    @Override
+    public Class getShadowNodeClass() {
+        return CastButtonShadowNode.class;
+    }
+
+    @ReactProp(name = ViewProps.COLOR, defaultDouble = Color.TRANSPARENT)
+    public void setColor(CastButton view, double color) {
+        view.setColor((int)color);
+    }
+
+    @Override
+    public void receiveCommand(CastButton view, int commandId, @Nullable ReadableArray args) {
         if(commandId == SHOW_DIALOG) {
-            root.showDialog();
+            view.showDialog();
         }
     }
 
@@ -38,5 +65,29 @@ public class CastButtonManager extends SimpleViewManager<CastButton> {
     @Override
     public Map<String, Integer> getCommandsMap() {
         return MapBuilder.of("showDialog", SHOW_DIALOG);
+    }
+
+
+    class CastButtonShadowNode extends LayoutShadowNode implements YogaMeasureFunction {
+        private int buttonWidth, buttonHeight;
+        private boolean measured = false;
+
+        public CastButtonShadowNode() {
+            setMeasureFunction(this);
+        }
+
+        @Override
+        public long measure(YogaNode node, float width, YogaMeasureMode widthMode, float height, YogaMeasureMode heightMode) {
+            if(!measured) {
+                int spec = View.MeasureSpec.makeMeasureSpec(LayoutParams.WRAP_CONTENT, MeasureSpec.UNSPECIFIED);
+                MediaRouteButton button = new MediaRouteButton(getThemedContext());
+                button.measure(spec, spec);
+                buttonWidth = button.getMeasuredWidth();
+                buttonHeight = button.getMeasuredHeight();
+                measured = true;
+            }
+
+            return YogaMeasureOutput.make(buttonWidth, buttonHeight);
+        }
     }
 }
