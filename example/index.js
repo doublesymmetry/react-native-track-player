3a25e25d91631225b70a3723537cbadf23105b8a
David Chavez <david@dcvz.io>
2018-04-05 00:37:44 +0200

    Add example project and first example

---
+++ example/index.js
@@ -0,0 +1,24 @@
+import { AppRegistry } from 'react-native';
+import TrackPlayer from 'react-native-track-player';
+
+import App from './App';
+import PlayerStore from './react/stores/Player';
+import TrackStore, { playbackStates } from './react/stores/Track';
+
+AppRegistry.registerComponent('example', () => App);
+
+TrackPlayer.registerEventHandler(async (data) => {
+  if (data.type === 'playback-track-changed') {
+    const track = await TrackPlayer.getTrack(data.nextTrack);
+    TrackStore.title = track.title;
+    TrackStore.artist = track.artist;
+    TrackStore.artwork = track.artwork;
+  } else if (data.type === 'playback-state') {
+    PlayerStore.playbackState = data.state;
+    if (data.state === TrackPlayer.STATE_BUFFERING || data.state === TrackPlayer.STATE_PLAYING) {
+      TrackStore.playbackState = playbackStates.playing;
+    } else {
+      TrackStore.playbackState = playbackStates.halted;
+    }
+  }
+});
