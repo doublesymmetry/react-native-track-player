55acfff0cb5b713aaec6b75608c44abe83f8ffd6
Guilherme Chaguri <guichaguri@gmail.com>
2019-01-14 22:33:11 -0200

    [android] Added null-safety for track IDs in skip. Closes #394

--- android/src/main/java/com/guichaguri/trackplayer/service/player/ExoPlayback.java
+++ android/src/main/java/com/guichaguri/trackplayer/service/player/ExoPlayback.java
@@ -65,6 +65,11 @@ public abstract class ExoPlayback<T extends Player> implements EventListener {
     }
 
     public void skip(String id, Promise promise) {
+        if(id == null || id.isEmpty()) {
+            promise.reject("invalid_id", "The ID can't be null or empty");
+            return;
+        }
+
         for(int i = 0; i < queue.size(); i++) {
             if(id.equals(queue.get(i).id)) {
                 lastKnownWindow = player.getCurrentWindowIndex();
