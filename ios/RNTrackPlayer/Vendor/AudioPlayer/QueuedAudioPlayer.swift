a4c104cde1228e744a8ff57fdb9d2ffb0a20da16
dnish <harbecke.dennis@gmail.com>
2019-02-19 22:32:18 +0100

    - Updated for array

--- ios/RNTrackPlayer/Vendor/AudioPlayer/QueuedAudioPlayer.swift
+++ ios/RNTrackPlayer/Vendor/AudioPlayer/QueuedAudioPlayer.swift
@@ -90,7 +90,9 @@ public class QueuedAudioPlayer: AudioPlayer {
     public func add(items: [AudioItem], playWhenReady: Bool = true) throws {
         if currentItem == nil {
             queueManager.addItems(items)
-            try self.load(item: currentItem!, playWhenReady: playWhenReady)
+            if playWhenReady == true { 
+             try self.load(item: currentItem!, playWhenReady: playWhenReady)
+            }
         }
         else {
             queueManager.addItems(items)
