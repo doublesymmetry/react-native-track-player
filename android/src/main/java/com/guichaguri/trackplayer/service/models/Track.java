ecf50fd7719ababb6ac5d9f9ad5b5d8ab37554cd
Abel Tesfaye <abeltesfaye45@gmail.com>
2019-03-12 19:09:26 +0300

    fixed NullPointerException when empty url is given

--- android/src/main/java/com/guichaguri/trackplayer/service/models/Track.java
+++ android/src/main/java/com/guichaguri/trackplayer/service/models/Track.java
@@ -113,7 +113,7 @@ public class Track {
         builder.putString(METADATA_KEY_ALBUM, album);
         builder.putString(METADATA_KEY_DATE, date);
         builder.putString(METADATA_KEY_GENRE, genre);
-        builder.putString(METADATA_KEY_MEDIA_URI, uri.toString());
+        builder.putString(METADATA_KEY_MEDIA_URI, (uri != null)?uri.toString():"");
         builder.putString(METADATA_KEY_MEDIA_ID, id);
 
         builder.putLong(METADATA_KEY_DURATION, duration);
