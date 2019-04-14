d91dec1ee623c50c60b9e0535201f386fab5c0b4
David Chavez <david@dcvz.io>
2019-04-14 22:37:46 +0200

    Update Track.swift

--- ios/RNTrackPlayer/Models/Track.swift
+++ ios/RNTrackPlayer/Models/Track.swift
@@ -10,7 +10,7 @@ import Foundation
 import MediaPlayer
 import AVFoundation
 
-public class Track: NSObject {
+class Track: NSObject {
     let id: String
     let url: MediaURL
     dynamic let title: String
