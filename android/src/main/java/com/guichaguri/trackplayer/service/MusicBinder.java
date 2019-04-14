493faee80548e2c0c1cff129fa28dcef4c7016ca
Pieterjan Van Saet <hybridfox2@gmail.com>
2019-04-14 22:48:19 +0200

    Add the ability to update trackMetadata (#331)

--- android/src/main/java/com/guichaguri/trackplayer/service/MusicBinder.java
+++ android/src/main/java/com/guichaguri/trackplayer/service/MusicBinder.java
@@ -4,6 +4,7 @@ import android.os.Binder;
 import android.os.Bundle;
 import com.facebook.react.bridge.Promise;
 import com.guichaguri.trackplayer.service.player.ExoPlayback;
+import com.guichaguri.trackplayer.service.models.Track;
 
 /**
  * @author Guichaguri
@@ -50,6 +51,10 @@ public class MusicBinder extends Binder {
 
     public int getRatingType() {
         return manager.getMetadata().getRatingType();
+    }
+
+    public void updateMetadata(Track track) {
+        manager.updateMetadata(track);
     }
 
     public void destroy() {
--- index.d.ts
+++ index.d.ts
@@ -131,6 +131,10 @@ declare namespace RNTrackPlayer {
   export function getBufferedPosition(): Promise<number>;
   export function getState(): Promise<State>;
   export function getRate(): Promise<number>;
+  
+  // Metadata
+
+  export function updateMetadata(track: Track): void;
 
   // Components
 
