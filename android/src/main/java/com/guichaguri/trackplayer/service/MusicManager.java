68a141f1370e9daa68fc71aa020208e6a02f02ff
Guilherme Chaguri <guichaguri@gmail.com>
2019-04-21 22:44:07 -0300

    Revert "Updated to RN 0.59.5, moved to androidx packages"
    This reverts commit 6471be68dfc772bc3f94ad74d74455902651a7c8.

--- android/src/main/java/com/guichaguri/trackplayer/service/MusicManager.java
+++ android/src/main/java/com/guichaguri/trackplayer/service/MusicManager.java
@@ -16,7 +16,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.PowerManager;
 import android.os.PowerManager.WakeLock;
-import androidx.annotation.RequiresApi;
+import android.support.annotation.RequiresApi;
 import android.util.Log;
 import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.DefaultLoadControl;
