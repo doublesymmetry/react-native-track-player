9240c6db6ad331b864289a137addf05d92ff2525
puckey <jonathan@puckey.studio>
2019-05-21 14:41:44 +0200

    [example] reflect buffered progress in progressbar

--- example/react/components/ProgressBar.js
+++ example/react/components/ProgressBar.js
@@ -8,15 +8,18 @@ import {
 import { useTrackPlayerProgress } from 'react-native-track-player';
 
 export function ProgressBar() {
-  const { position, duration } = useTrackPlayerProgress();
+  const { position, bufferedPosition, duration } = useTrackPlayerProgress(100);
   const progress = duration
     ? position / duration
     : 0;
-  ;
+  const bufferedProgress = duration
+    ? bufferedPosition / duration
+    : 0;
   return (
     <View style={styles.progress}>
       <View style={{ flex: progress, backgroundColor: 'red' }} />
-      <View style={{ flex: 1 - progress, backgroundColor: 'grey' }} />
+      <View style={{ flex: bufferedProgress - progress, backgroundColor: 'grey' }} />
+      <View style={{ flex: 1 - bufferedProgress, backgroundColor: 'lightgray' }} />
     </View>
   );
 }
