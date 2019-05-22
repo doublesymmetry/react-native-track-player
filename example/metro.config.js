7cda532513d26497e45d58b17ef1cb5ccb1b9a8f
puckey <jonathan@puckey.studio>
2019-05-22 11:10:21 +0200

    [example] update to react-native 0.59.8

---
+++ example/metro.config.js
@@ -0,0 +1,23 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+const blacklist = require('metro-config/src/defaults/blacklist');
+module.exports = {
+  resolver:{
+    blacklistRE: blacklist([
+      /node_modules\/.*\/node_modules\/react-native\/.*/
+    ])
+  },
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+};
