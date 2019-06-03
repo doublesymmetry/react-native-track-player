da407573456c71338b8e11c132844e4ac9b1cc0f
David Chavez <david@dcvz.io>
2019-06-03 18:39:34 +0200

    Submodule for Vendored AudioPlayer & Updated example RN 0.59 with Hooks (#615)
    * Use submodule for vendored Audio player & update example
    * Remove vendored audioplayer
    * Add vendored audioplayer as submodule
    * Update audio player branch to fork

--- example/App.js
+++ example/App.js
@@ -1,47 +1,23 @@
-import React, { Component } from 'react';
-import TrackPlayer from 'react-native-track-player';
-import { StackNavigator } from 'react-navigation';
+import React from "react";
+import { createStackNavigator, createAppContainer } from "react-navigation";
 
-import PlayerStore from './react/stores/Player';
-import TrackStore from './react/stores/Track';
+import LandingScreen from "./react/screens/LandingScreen";
+import PlaylistScreen from "./react/screens/PlaylistScreen";
 
-import LandingScreen from './react/screens/LandingScreen';
-import PlaylistScreen from './react/screens/PlaylistScreen';
-
-const RootStack = StackNavigator({
-  Landing: {
-    screen: LandingScreen,
+const AppNavigator = createStackNavigator(
+  {
+    Landing: {
+      screen: LandingScreen
+    },
+    Playlist: {
+      screen: PlaylistScreen
+    }
   },
-  Playlist: {
-    screen: PlaylistScreen,
-  },
-}, { initialRouteName: 'Landing' })
+  { initialRouteName: "Landing" }
+);
 
-export default class App extends Component {
+const AppContainer = createAppContainer(AppNavigator);
 
-  componentDidMount() {
-    this._onTrackChanged = TrackPlayer.addEventListener('playback-track-changed', async (data) => {
-      if (data.nextTrack) {
-        const track = await TrackPlayer.getTrack(data.nextTrack);
-        TrackStore.title = track.title;
-        TrackStore.artist = track.artist;
-        TrackStore.artwork = track.artwork;
-      }
-    })
-
-    this._onStateChanged = TrackPlayer.addEventListener('playback-state', (data) => {
-      PlayerStore.playbackState = data.state;
-    })
-  }
-
-  componentWillUnmount() {
-    this._onTrackChanged.remove()
-    this._onStateChanged.remove()
-  }
-
-  render() {
-    return (
-      <RootStack />
-    );
-  }
+export default function App() {
+  return <AppContainer />;
 }
--- example/react/screens/LandingScreen.js
+++ example/react/screens/LandingScreen.js
@@ -1,26 +1,18 @@
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity
-} from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
-import Screen from '../components/Screen';
+import Screen from "../components/Screen";
 
 export default class LandingScreen extends Screen {
   static navigationOptions = {
-    title: 'React Native Track Player',
+    title: "React Native Track Player"
   };
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.header}>
-          Example Demos
-        </Text>
-        <TouchableOpacity onPress={() => this.navigateTo('Playlist')}>
+        <Text style={styles.header}>Example Demos</Text>
+        <TouchableOpacity onPress={() => this.navigateTo("Playlist")}>
           <Text>Playlist Example</Text>
         </TouchableOpacity>
       </View>
@@ -31,14 +23,14 @@ export default class LandingScreen extends Screen {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   header: {
     fontSize: 20,
     marginTop: 20,
     marginBottom: 10,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
+    fontWeight: "bold",
+    textAlign: "center"
+  }
 });
