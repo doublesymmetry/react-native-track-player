cefd1d5e4b846d33354f48f0827a79858f1e2ed3
David Chavez <david@dcvz.io>
2019-06-24 21:30:04 +0200

    [ios] Separate control of playback notification metadata

--- index.d.ts
+++ index.d.ts
@@ -42,7 +42,7 @@ declare namespace RNTrackPlayer {
   type ServiceHandler = () => Promise<void>;
   export function registerPlaybackService(serviceFactory: () => ServiceHandler): void;
 
-  type EmitterSubscription = { remove: () => void; };
+  interface EmitterSubscription { remove: () => void; }
   export function addEventListener(type: EventType, listener: (data: any) => void): EmitterSubscription;
 
   export interface TrackMetadata {
@@ -67,6 +67,15 @@ declare namespace RNTrackPlayer {
     [key: string]: any;
   }
 
+  interface NowPlayingMetadata {
+    title?: string;
+    album?: string;
+    artist?: string;
+    duration?: number;
+    elapsedTime?: number;
+    artwork?: string | ResourceObject;
+  }
+
   export interface PlayerOptions {
     minBuffer?: number;
     maxBuffer?: number;
@@ -74,7 +83,7 @@ declare namespace RNTrackPlayer {
     maxCacheSize?: number;
     iosCategory?: 'playback' | 'playAndRecord' | 'multiRoute' | 'ambient' | 'soloAmbient' | 'record';
     iosCategoryMode?: 'default' | 'gameChat' | 'measurement' | 'moviePlayback' | 'spokenAudio' | 'videoChat' | 'videoRecording' | 'voiceChat' | 'voicePrompt';
-    iosCategoryOptions?: Array<'mixWithOthers' | 'duckOthers' | 'interruptSpokenAudioAndMixWithOthers' | 'allowBluetooth' | 'allowBluetoothA2DP' | 'allowAirPlay' | 'defaultToSpeaker'>;
+    iosCategoryOptions?: ('mixWithOthers' | 'duckOthers' | 'interruptSpokenAudioAndMixWithOthers' | 'allowBluetooth' | 'allowBluetoothA2DP' | 'allowAirPlay' | 'defaultToSpeaker')[];
     waitForBuffer?: boolean;
   }
 
@@ -124,8 +133,11 @@ declare namespace RNTrackPlayer {
   export function removeUpcomingTracks(): Promise<void>;
 
   // Control Center / Notification Metadata Commands
+  
   export function updateOptions(options: MetadataOptions): void;
   export function updateMetadataForTrack(id: string, metadata: TrackMetadata) : Promise<void>;
+  export function clearNowPlayingMetadata(): Promise<void>
+  export function updateNowPlayingMetadata(metadata: NowPlayingMetadata): Promise<void>
 
   // Player Playback Commands
 
