4341f7740292b7004756bcb54446e08c86841638
David Chavez <david@dcvz.io>
2019-07-10 14:17:32 +0200

    Address review comments

--- ios/RNTrackPlayer/Utils/Metadata.swift
+++ ios/RNTrackPlayer/Utils/Metadata.swift
@@ -10,7 +10,10 @@ import Foundation
 import MediaPlayer
 
 struct Metadata {
+    private static var currentImageTask: URLSessionDataTask?
+
     static func update(for player: AudioPlayer, with metadata: [String: Any]) {
+        currentImageTask?.cancel()
         var ret: [NowPlayingInfoKeyValue] = []
         
         if let title = metadata["title"] as? String {
@@ -36,14 +39,16 @@ struct Metadata {
         player.nowPlayingInfoController.set(keyValues: ret)
         
         if let artworkURL = MediaURL(object: metadata["artwork"]) {
-            URLSession.shared.dataTask(with: artworkURL.value, completionHandler: { [weak player] (data, _, error) in
+            currentImageTask = URLSession.shared.dataTask(with: artworkURL.value, completionHandler: { [weak player] (data, _, error) in
                 if let data = data, let image = UIImage(data: data), error == nil {
                     let artwork = MPMediaItemArtwork(boundsSize: image.size, requestHandler: { (size) -> UIImage in
                         return image
                     })
                     player?.nowPlayingInfoController.set(keyValue: MediaItemProperty.artwork(artwork))
                 }
-            }).resume()
+            })
+            
+            currentImageTask?.resume()
         }
     }
 }
