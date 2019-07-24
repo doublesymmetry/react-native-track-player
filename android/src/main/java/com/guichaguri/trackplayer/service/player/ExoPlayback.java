b55111a825c16d82832c9fbd7a7fcd0e72d41264
Hiroki Sato <m@skahack.com>
2019-07-24 22:26:56 +0900

    reset lastKnownWindow

--- android/src/main/java/com/guichaguri/trackplayer/service/player/ExoPlayback.java
+++ android/src/main/java/com/guichaguri/trackplayer/service/player/ExoPlayback.java
@@ -243,7 +243,7 @@ public abstract class ExoPlayback<T extends Player> implements EventListener, Me
         Log.d(Utils.LOG, "onPositionDiscontinuity: " + reason);
 
         if(lastKnownWindow != player.getCurrentWindowIndex()) {
-            Track previous = lastKnownWindow == C.INDEX_UNSET ? null : queue.get(lastKnownWindow);
+            Track previous = lastKnownWindow == C.INDEX_UNSET || lastKnownWindow >= queue.size() ? null : queue.get(lastKnownWindow);
             Track next = getCurrentTrack();
 
             // Track changed because it ended
