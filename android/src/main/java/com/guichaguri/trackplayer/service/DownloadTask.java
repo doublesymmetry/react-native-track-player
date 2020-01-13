c2c3bac24627c0f33c4cefddb62ed110074d83c1
Drazail <rezay3@gmail.com>
2020-01-13 10:45:31 +0330

    adding download-progress and download-completed events

--- android/src/main/java/com/guichaguri/trackplayer/service/DownloadTask.java
+++ android/src/main/java/com/guichaguri/trackplayer/service/DownloadTask.java
@@ -3,6 +3,7 @@ package com.guichaguri.trackplayer.service;
 import android.content.Context;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.util.Log;
 
 import com.google.android.exoplayer2.upstream.DataSpec;
@@ -10,6 +11,7 @@ import com.google.android.exoplayer2.upstream.DefaultHttpDataSource;
 import com.google.android.exoplayer2.upstream.cache.Cache;
 import com.google.android.exoplayer2.upstream.cache.CacheDataSource;
 import com.google.android.exoplayer2.util.Util;
+import com.guichaguri.trackplayer.module.MusicEvents;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -17,6 +19,7 @@ import java.io.IOException;
 
 class TaskParams {
     Context ctx;
+    MusicService service;
     Cache cache;
     String key;
     Uri uri;
@@ -24,8 +27,9 @@ class TaskParams {
     String path;
     boolean ForceOverWrite;
 
-    TaskParams(Context ctx, Cache cache, String key, Uri uri, int length, String path, boolean ForceOverWrite) {
+    TaskParams(Context ctx, MusicService service, Cache cache, String key, Uri uri, int length, String path, boolean ForceOverWrite) {
         this.ctx = ctx;
+        this.service = service;
         this.cache = cache;
         this.key = key;
         this.uri = uri;
@@ -36,21 +40,29 @@ class TaskParams {
 }
 
 public class DownloadTask extends AsyncTask<TaskParams, Integer, String> {
+
+    MusicService service;
+    String key;
+    Uri uri;
+    int length;
+
+    int progress = 0;
+
     @Override
     protected String doInBackground (TaskParams ... params) {
 
+        service = params[0].service;
         Context ctx = params[0].ctx;
         Cache cache= params[0].cache;
-        String key= params[0].key;
-        Uri uri= params[0].uri;
-        int length= params[0].length;
+        key= params[0].key;
+        uri= params[0].uri;
+        length= params[0].length;
         String path= params[0].path;
         boolean ForceOverWrite= params[0].ForceOverWrite;
         String userAgent = Util.getUserAgent(ctx, "react-native-track-player");
         Log.d(Utils.LOG, "cache download : getUserAgent: " + userAgent + "//");
-        // the buffer which hosts bytes read from cachedDatasource
-        byte[] buffer = new byte[1024];
-        // the buffer which should host all the data
+        byte[] buffer = new byte[102400];
+
 
         DefaultHttpDataSource ds = new DefaultHttpDataSource(
                 userAgent
@@ -65,19 +77,23 @@ public class DownloadTask extends AsyncTask<TaskParams, Integer, String> {
                 int read = 0;
                 while( (read = dataSource.read(buffer,0, buffer.length)) > 0) {
                     fs.write(buffer, 0, read);
+                    publishProgress(read);
                 }
                 fs.close();
                 dataSource.close();
                 ds.close();
+                return (file.getAbsolutePath());
             } else {
                 FileOutputStream fs = new FileOutputStream(path);
                 int read = 0;
                 while( (read = dataSource.read(buffer,0, buffer.length)) > 0) {
                     fs.write(buffer, 0, read);
+                    publishProgress(read);
                 }
                 fs.close();
                 dataSource.close();
                 ds.close();
+                return (file.getAbsolutePath());
             }
 
 
@@ -90,4 +106,24 @@ public class DownloadTask extends AsyncTask<TaskParams, Integer, String> {
         return "0";
     }
 
+    @Override
+    protected void onProgressUpdate(Integer... prog) {
+        progress = progress + prog[0];
+        Bundle bundle = new Bundle();
+        bundle.putString("key", key);
+        bundle.putInt("progress", progress);
+        bundle.putInt("length", length);
+        bundle.putString("url", uri.toString());
+        service.emit(MusicEvents.DOWNLOAD_PROGRESS, bundle);
+    }
+
+    @Override
+    protected void onPostExecute(String path) {
+        Bundle bundle = new Bundle();
+        bundle.putString("key", key);
+        bundle.putInt("length", length);
+        bundle.putString("url", uri.toString());
+        bundle.putString("path", path);
+        service.emit(MusicEvents.DOWNLOAD_COMPLETED, bundle);
+    }
 }
