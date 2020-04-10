38c8592b7d0ef8b82413b690c17801a6cf0f1bd3
Dustin Bahr <dustin@curiousmedia.com>
2020-04-10 15:57:12 -0600

    [iOS] Fix issues with auto advancing in background, and remote commands.
    Extend QueuedAudioPlayer in order to send 'playback' events at appropriate times instead of relying on asynchronous events emitted by SwiftAudio.
    
    This improves the reliability of the information included in 'playback' events, and also simplifies the code for them.

--- ios/RNTrackPlayer/RNTrackPlayer.swift
+++ ios/RNTrackPlayer/RNTrackPlayer.swift
@@ -16,13 +16,9 @@ public class RNTrackPlayer: RCTEventEmitter {
     
     private var hasInitialized = false
 
-    private lazy var player: QueuedAudioPlayer = {
-        let player = QueuedAudioPlayer()
+    private lazy var player: RNTrackPlayerAudioPlayer = {
+        let player = RNTrackPlayerAudioPlayer(reactEventEmitter: self)
         player.bufferDuration = 1
-
-        // disable auto advance, so that we can control the order of
-        // operations in order to send accurate event data
-        player.automaticallyPlayNextSong = false
 
         return player
     }()
@@ -177,46 +173,7 @@ public class RNTrackPlayer: RCTEventEmitter {
         
         try? AVAudioSession.sharedInstance().setCategory(sessionCategory, mode: sessionCategoryMode, options: sessionCategoryOptions)
         
-        
         // setup event listeners
-        player.event.stateChange.addListener(self) { [weak self] state in
-            self?.sendEvent(withName: "playback-state", body: ["state": state.rawValue])
-        }
-        
-        player.event.fail.addListener(self) { [weak self] error in
-            self?.sendEvent(withName: "playback-error", body: ["error": error?.localizedDescription])
-        }
-        
-        player.event.playbackEnd.addListener(self) { [weak self] reason in
-            guard let `self` = self else { return }
-
-            if reason == .playedUntilEnd {
-                // playbackEnd is called twice at the end of a track;
-                // we ignore .skippedToNext and only fire an event
-                // for .playedUntilEnd
-                // nextTrack might be nil if there are no more, but still send the event for consistency
-                self.sendEvent(withName: "playback-track-changed", body: [
-                    "track": (self.player.currentItem as? Track)?.id,
-                    "position": self.player.currentTime,
-                    "nextTrack": (self.player.nextItems.first as? Track)?.id,
-                    ])
-                
-                if self.player.nextItems.count == 0 {
-                    // fire an event for the queue ending
-                    self.sendEvent(withName: "playback-queue-ended", body: [
-                        "track": (self.player.currentItem as? Track)?.id,
-                        "position": self.player.currentTime,
-                        ])
-                } else {
-                    // we are not using automaticallyPlayNextSong on the player in order
-                    // to be in control of specifically when the above events are sent
-                    // so, attempt to go to the next track now
-                    try? self.player.next() 
-                }
-            }
-
-        }
-        
         player.remoteCommandController.handleChangePlaybackPositionCommand = { [weak self] event in
             if let event = event as? MPChangePlaybackPositionCommandEvent {
                 self?.sendEvent(withName: "remote-seek", body: ["position": event.positionTime])
@@ -317,6 +274,11 @@ public class RNTrackPlayer: RCTEventEmitter {
                                           bookmarkOptions: options["bookmarkOptions"] as? [String: Any])
         }
 
+        // SwiftAudio AudioPlayer re-enables the commands on track load.
+        // Update player.remoteCommands, so that the correct commands will be re-enabled.
+        player.remoteCommands = remoteCommands
+
+        // Enable the commands immediately, otherwise they would only be enabled on track load.
         player.enableRemoteCommands(remoteCommands)
         
         resolve(NSNull())
@@ -349,14 +311,6 @@ public class RNTrackPlayer: RCTEventEmitter {
             
             try? player.add(items: tracks, at: insertIndex)
         } else {
-            if (player.currentItem == nil && tracks.count > 0) {
-                sendEvent(withName: "playback-track-changed", body: [
-                    "track": nil,
-                    "position": 0,
-                    "nextTrack": tracks.first!.id
-                ])
-            }
-            
             try? player.add(items: tracks, playWhenReady: false)
         }
         
@@ -397,12 +351,6 @@ public class RNTrackPlayer: RCTEventEmitter {
             return
         }
         
-        sendEvent(withName: "playback-track-changed", body: [
-            "track": (player.currentItem as? Track)?.id,
-            "position": player.currentTime,
-            "nextTrack": trackId,
-        ])
-        
         print("Skipping to track:", trackId)
         try? player.jumpToItem(atIndex: trackIndex, playWhenReady: player.playerState == .playing)
         resolve(NSNull())
@@ -412,11 +360,6 @@ public class RNTrackPlayer: RCTEventEmitter {
     public func skipToNext(resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
         print("Skipping to next track")
         do {
-            sendEvent(withName: "playback-track-changed", body: [
-                "track": (player.currentItem as? Track)?.id,
-                "position": player.currentTime,
-                "nextTrack": (player.nextItems.first as? Track)?.id,
-            ])
             try player.next()
             resolve(NSNull())
         } catch (_) {
@@ -428,11 +371,6 @@ public class RNTrackPlayer: RCTEventEmitter {
     public func skipToPrevious(resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
         print("Skipping to next track")
         do {
-            sendEvent(withName: "playback-track-changed", body: [
-                "track": (player.currentItem as? Track)?.id,
-                "position": player.currentTime,
-                "nextTrack": (player.previousItems.last as? Track)?.id,
-            ])
             try player.previous()
             resolve(NSNull())
         } catch (_) {
