e93ee16b798ec8571b14c8ddf98d34a8418f5ff5
Dustin Bahr <dustin@curiousmedia.com>
2020-04-20 08:50:31 -0600

    Add missing override keyword

--- ios/RNTrackPlayer/RNTrackPlayerAudioPlayer.swift
+++ ios/RNTrackPlayer/RNTrackPlayerAudioPlayer.swift
@@ -57,7 +57,7 @@ public class RNTrackPlayerAudioPlayer: QueuedAudioPlayer {
      - parameter item: The AudioItem to load. The info given in this item is the one used for the InfoCenter.
      - parameter playWhenReady: Immediately start playback when the item is ready. Default is `true`. If you disable this you have to call play() or togglePlay() when the `state` switches to `ready`.
      */
-    public func load(item: AudioItem, playWhenReady: Bool = true) throws {
+    public override func load(item: AudioItem, playWhenReady: Bool = true) throws {
         let url: URL
         switch item.getSourceType() {
         case .stream:
