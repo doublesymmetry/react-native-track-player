203d53b40a722305c8509ddc926025f8c112637f
MrMeo <2053300+minhtc@users.noreply.github.com>
2020-04-30 22:38:53 +0700

    [ts] Add missing option types (#768)

--- index.d.ts
+++ index.d.ts
@@ -98,6 +98,7 @@ declare namespace RNTrackPlayer {
     dislikeOptions?: FeedbackOptions;
     bookmarkOptions?: FeedbackOptions;
     stopWithApp?: boolean;
+    alwaysPauseOnInterruption?: boolean; // default: false
 
     capabilities?: Capability[];
     notificationCapabilities?: Capability[];
