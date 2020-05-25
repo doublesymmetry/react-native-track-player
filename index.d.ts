0bf5a801e044c8b162af31bc7198ee495084d3cf
André Kovac <me@andrekovac.com>
2020-05-25 13:54:39 +0200

    Run prettier on typescript type definitions file

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,8 @@
-import { Component } from 'react';
+import { Component } from "react";
 
 export = RNTrackPlayer;
 
 declare namespace RNTrackPlayer {
-
   export type EventType =
     | "playback-state"
     | "playback-error"
@@ -27,11 +26,7 @@ declare namespace RNTrackPlayer {
     | "remote-dislike"
     | "remote-bookmark";
 
-  export type TrackType =
-    | "default"
-    | "dash"
-    | "hls"
-    | "smoothstreaming";
+  export type TrackType = "default" | "dash" | "hls" | "smoothstreaming";
 
   type ResourceObject = any;
 
@@ -55,10 +50,15 @@ declare namespace RNTrackPlayer {
   export function registerEventHandler(handler: EventHandler): void;
 
   type ServiceHandler = () => Promise<void>;
-  export function registerPlaybackService(serviceFactory: () => ServiceHandler): void;
+  export function registerPlaybackService(
+    serviceFactory: () => ServiceHandler
+  ): void;
 
-  type EmitterSubscription = { remove: () => void; };
-  export function addEventListener(type: EventType, listener: (data: any) => void): EmitterSubscription;
+  type EmitterSubscription = { remove: () => void };
+  export function addEventListener(
+    type: EventType,
+    listener: (data: any) => void
+  ): EmitterSubscription;
 
   export interface TrackMetadata {
     duration?: number;
@@ -88,18 +88,41 @@ declare namespace RNTrackPlayer {
     playBuffer?: number;
     backBuffer?: number;
     maxCacheSize?: number;
-    iosCategory?: 'playback' | 'playAndRecord' | 'multiRoute' | 'ambient' | 'soloAmbient' | 'record';
-    iosCategoryMode?: 'default' | 'gameChat' | 'measurement' | 'moviePlayback' | 'spokenAudio' | 'videoChat' | 'videoRecording' | 'voiceChat' | 'voicePrompt';
-    iosCategoryOptions?: Array<'mixWithOthers' | 'duckOthers' | 'interruptSpokenAudioAndMixWithOthers' | 'allowBluetooth' | 'allowBluetoothA2DP' | 'allowAirPlay' | 'defaultToSpeaker'>;
+    iosCategory?:
+      | "playback"
+      | "playAndRecord"
+      | "multiRoute"
+      | "ambient"
+      | "soloAmbient"
+      | "record";
+    iosCategoryMode?:
+      | "default"
+      | "gameChat"
+      | "measurement"
+      | "moviePlayback"
+      | "spokenAudio"
+      | "videoChat"
+      | "videoRecording"
+      | "voiceChat"
+      | "voicePrompt";
+    iosCategoryOptions?: Array<
+      | "mixWithOthers"
+      | "duckOthers"
+      | "interruptSpokenAudioAndMixWithOthers"
+      | "allowBluetooth"
+      | "allowBluetoothA2DP"
+      | "allowAirPlay"
+      | "defaultToSpeaker"
+    >;
     waitForBuffer?: boolean;
   }
 
   interface FeedbackOptions {
     /** Marks wether the option should be marked as active or "done" */
-    isActive: boolean
+    isActive: boolean;
 
     /** The title to give the action (relevant for iOS) */
-    title: string
+    title: string;
   }
 
   export interface MetadataOptions {
@@ -133,7 +156,10 @@ declare namespace RNTrackPlayer {
 
   // Player Queue Commands
 
-  export function add(tracks: Track | Track[], insertBeforeId?: string): Promise<void>;
+  export function add(
+    tracks: Track | Track[],
+    insertBeforeId?: string
+  ): Promise<void>;
   export function remove(trackIds: string | string[]): Promise<void>;
   export function skip(trackId: string): Promise<void>;
   export function skipToNext(): Promise<void>;
@@ -142,7 +168,10 @@ declare namespace RNTrackPlayer {
 
   // Control Center / Notification Metadata Commands
   export function updateOptions(options: MetadataOptions): Promise<void>;
-  export function updateMetadataForTrack(id: string, metadata: TrackMetadata) : Promise<void>;
+  export function updateMetadataForTrack(
+    id: string,
+    metadata: TrackMetadata
+  ): Promise<void>;
 
   // Player Playback Commands
 
@@ -174,7 +203,10 @@ declare namespace RNTrackPlayer {
     duration: number;
   }
 
-  export class ProgressComponent<P = {}, S = {}> extends Component<P, ProgressComponentState & S> {
+  export class ProgressComponent<P = {}, S = {}> extends Component<
+    P,
+    ProgressComponentState & S
+  > {
     public getProgress: () => number;
     public getBufferedProgress: () => number;
   }
