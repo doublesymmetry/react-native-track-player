4ba18141b7e1e6c81fae699a9045dd1111c9f617
Jonathan Puckey <me@jonathanpuckey.com>
2020-06-01 10:09:08 +0200

    [ios] fix race conditions (close #652) (#956)
    * [ios] fix race conditions (close #652)
    
    * clean whitespace

--- ios/RNTrackPlayer/Vendor/SwiftAudio/Classes/NowPlayingInfoController/NowPlayingInfoController.swift
+++ ios/RNTrackPlayer/Vendor/SwiftAudio/Classes/NowPlayingInfoController/NowPlayingInfoController.swift
@@ -8,43 +8,47 @@
 import Foundation
 import MediaPlayer
 
-
 public class NowPlayingInfoController: NowPlayingInfoControllerProtocol {
-    
+
     private var _infoCenter: NowPlayingInfoCenter
     private var _info: [String: Any] = [:]
-    
+
     var infoCenter: NowPlayingInfoCenter {
         return _infoCenter
     }
-    
+
     var info: [String: Any] {
         return _info
     }
-    
+
     public required init() {
         self._infoCenter = MPNowPlayingInfoCenter.default()
     }
-    
+
     public required init(infoCenter: NowPlayingInfoCenter) {
         self._infoCenter = infoCenter
     }
-    
+
     public func set(keyValues: [NowPlayingInfoKeyValue]) {
-        keyValues.forEach { (keyValue) in
-            _info[keyValue.getKey()] = keyValue.getValue()
+        DispatchQueue.main.async {
+            keyValues.forEach { (keyValue) in
+                self._info[keyValue.getKey()] = keyValue.getValue()
+            }
+            self._infoCenter.nowPlayingInfo = self._info
         }
-        self._infoCenter.nowPlayingInfo = _info
     }
-    
+
     public func set(keyValue: NowPlayingInfoKeyValue) {
-        _info[keyValue.getKey()] = keyValue.getValue()
-        self._infoCenter.nowPlayingInfo = _info
+        DispatchQueue.main.async {
+            self._info[keyValue.getKey()] = keyValue.getValue()
+            self._infoCenter.nowPlayingInfo = self._info
+        }
     }
-    
+
     public func clear() {
-        self._info = [:]
-        self._infoCenter.nowPlayingInfo = _info
+        DispatchQueue.main.async {
+            self._info = [:]
+            self._infoCenter.nowPlayingInfo = self._info
+        }
     }
-    
 }
