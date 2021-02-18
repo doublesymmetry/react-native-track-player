3debb83a5f85a1eb7fec991d1eabd1e356db3e07
emmanuel.john <emmanuel.john@singlewire.com>
2021-02-18 09:43:31 -0600

    fix: smtc - only retrieve music properties after media type has been set
    Also updated windows project so it works from within example app and removed unused nuget package.

--- WindowsExample/react/components/Screen.tsx
+++
@@ -1,7 +0,0 @@
-import { Component } from 'react';
-
-export default class Screen extends Component<{navigation: any}> {
-  navigateTo = (path: any) => {
-    this.props.navigation.navigate(path);
-  };
-}
