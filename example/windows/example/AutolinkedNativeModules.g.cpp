286e8bdf68636e1d6bc9fc8d4b52dd403c19cdab
David Chavez <david@dcvz.io>
2021-06-01 21:13:01 +0200

    Use slider that supports windows

--- example/windows/example/AutolinkedNativeModules.g.cpp
+++ example/windows/example/AutolinkedNativeModules.g.cpp
@@ -3,6 +3,9 @@
 #include "pch.h"
 #include "AutolinkedNativeModules.g.h"
 
+// Includes from @react-native-community/slider
+#include <winrt/SliderWindows.h>
+
 // Includes from react-native-track-player
 #include <winrt/RNTrackPlayer.h>
 
@@ -11,6 +14,8 @@ namespace winrt::Microsoft::ReactNative
 
 void RegisterAutolinkedNativeModulePackages(winrt::Windows::Foundation::Collections::IVector<winrt::Microsoft::ReactNative::IReactPackageProvider> const& packageProviders)
 { 
+    // IReactPackageProviders from @react-native-community/slider
+    packageProviders.Append(winrt::SliderWindows::ReactPackageProvider());
     // IReactPackageProviders from react-native-track-player
     packageProviders.Append(winrt::RNTrackPlayer::ReactPackageProvider());
 }
