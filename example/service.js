17253c391f567ab9871616b62211836f5fa10265
Milen Pivchev <milen.pivchev@gmail.com>
2021-09-27 14:30:08 +0200

    Update service.js

--- example/service.js
+++ example/service.js
@@ -1,17 +1,19 @@
 import TrackPlayer, {Event, State} from 'react-native-track-player';
 
-let wasPausedByDuck = false;
-
 module.exports = async function setup() {
-  // TrackPlayer.addEventListener(Event.RemotePause, () => {
-  //   TrackPlayer.pause();
-  // });
+  TrackPlayer.addEventListener(Event.RemotePause, () => {
+    TrackPlayer.pause();
+  });
 
-  // TrackPlayer.addEventListener(Event.RemotePlay, () => {
-  //   TrackPlayer.play();
-  // });
+  TrackPlayer.addEventListener(Event.RemotePlay, () => {
+    TrackPlayer.play();
+  });
 
   TrackPlayer.addEventListener(Event.RemoteDuck, () => {
     TrackPlayer.pause()
   });
+
+  TrackPlayer.addEventListener(Event.RemoteNext, () => {
+    TrackPlayer.skipToNext()
+  })
 }
