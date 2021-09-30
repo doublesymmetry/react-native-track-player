46cd8dd695f18c1615e5e4a4d2af6078c632b3a4
David Chavez <david@dcvz.io>
2021-09-30 10:32:49 +0200

    Fix support for custom media types + additional track options (#1292)

--- android/src/main/java/com/guichaguri/trackplayer/model/Track.kt
+++ android/src/main/java/com/guichaguri/trackplayer/model/Track.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.support.v4.media.MediaDescriptionCompat
 import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaSessionCompat
+import com.doublesymmetry.kotlinaudio.models.AudioItemOptions
 import com.doublesymmetry.kotlinaudio.models.MediaType
 import com.google.android.exoplayer2.upstream.RawResourceDataSource
 import com.guichaguri.trackplayer.service_old.Utils
@@ -16,8 +17,8 @@ import java.util.*
  */
 class Track(context: Context, bundle: Bundle, ratingType: Int) : TrackMetadata() {
     var uri: Uri? = null
-    var resourceId: Int
-    var type = TrackType.DEFAULT
+    var resourceId: Int?
+    var type = MediaType.DEFAULT
     var contentType: String?
     var userAgent: String?
     var originalItem: Bundle?
@@ -35,97 +36,33 @@ class Track(context: Context, bundle: Bundle, ratingType: Int) : TrackMetadata()
         return builder
     }
 
-    fun toQueueItem(): MediaSessionCompat.QueueItem {
-        val descr = MediaDescriptionCompat.Builder()
-            .setTitle(title)
-            .setSubtitle(artist)
-            .setMediaUri(uri)
-            .setIconUri(artwork)
-            .build()
-        return MediaSessionCompat.QueueItem(descr, queueId)
-    }
-
-//    fun toMediaSource(ctx: Context?, playback: LocalPlayback): MediaSource {
-//        // Updates the user agent if not set
-//        if (userAgent == null || userAgent!!.isEmpty()) userAgent = Util.getUserAgent(
-//            ctx!!, "react-native-track-player"
-//        )
-//        val ds: DataSource.Factory?
-//        ds = if (resourceId != 0) {
-//            try {
-//                val raw = RawResourceDataSource(ctx!!)
-//                raw.open(DataSpec(uri!!))
-//                DataSource.Factory { raw }
-//            } catch (ex: IOException) {
-//                // Should never happen
-//                throw RuntimeException(ex)
-//            }
-//        } else if (Utils.isLocal(uri)) {
-//
-//            // Creates a local source factory
-//            DefaultDataSourceFactory(ctx!!, userAgent)
-//        } else {
-//
-//            // Creates a default http source factory, enabling cross protocol redirects
-//            val factory = DefaultHttpDataSourceFactory(
-//                userAgent, null,
-//                DefaultHttpDataSource.DEFAULT_CONNECT_TIMEOUT_MILLIS,
-//                DefaultHttpDataSource.DEFAULT_READ_TIMEOUT_MILLIS,
-//                true
-//            )
-//            if (headers != null) {
-//                factory.defaultRequestProperties.set(headers!!.toMap())
-//            }
-//            playback.enableCaching(factory)
-//        }
-//        return when (type) {
-//            TrackType.DASH -> createDashSource(ds)
-//            TrackType.HLS -> createHlsSource(ds)
-//            TrackType.SMOOTH_STREAMING -> createSsSource(ds)
-//            else -> ProgressiveMediaSource.Factory(
-//                ds, DefaultExtractorsFactory()
-//                    .setConstantBitrateSeekingEnabled(true)
-//            )
-//                .createMediaSource(uri!!)
-//        }
-//    }
-
-//    private fun createDashSource(factory: DataSource.Factory?): MediaSource {
-//        return DashMediaSource.Factory(DefaultDashChunkSource.Factory(factory!!), factory)
-//            .createMediaSource(uri!!)
-//    }
-//
-//    private fun createHlsSource(factory: DataSource.Factory?): MediaSource {
-//        return HlsMediaSource.Factory(factory!!)
-//            .createMediaSource(uri!!)
-//    }
-//
-//    private fun createSsSource(factory: DataSource.Factory?): MediaSource {
-//        return SsMediaSource.Factory(DefaultSsChunkSource.Factory(factory!!), factory)
-//            .createMediaSource(uri!!)
-//    }
-
     fun toAudioItem(): TrackAudioItem {
         return TrackAudioItem(
             this,
-            MediaType.DEFAULT,
+            type,
             uri.toString(),
             artist,
             title,
             album,
-            artwork.toString()
+            artwork.toString(),
+            AudioItemOptions(
+                headers,
+                userAgent,
+                resourceId
+            )
         )
     }
 
     init {
         resourceId = Utils.getRawResourceId(context, bundle, "url")
         uri = if (resourceId == 0) {
+            resourceId = null
             Utils.getUri(context, bundle, "url")
         } else {
-            RawResourceDataSource.buildRawResourceUri(resourceId)
+            RawResourceDataSource.buildRawResourceUri(resourceId!!)
         }
         val trackType = bundle.getString("type", "default")
-        for (t in TrackType.values()) {
+        for (t in MediaType.values()) {
             if (t.name.equals(trackType, ignoreCase = true)) {
                 type = t
                 break
