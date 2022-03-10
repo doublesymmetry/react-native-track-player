ba0843d8e2224dd06f1c97e546ae5752753d51b3
Jens Andersson <jens@fritan.com>
2022-03-10 15:54:31 +0100

    Don't start playing after seek if player is paused

--- android/src/main/java/com/doublesymmetry/trackplayer/service/MusicService.kt
+++ android/src/main/java/com/doublesymmetry/trackplayer/service/MusicService.kt
@@ -212,7 +212,7 @@ class MusicService : HeadlessJsTaskService() {
     }
 
     fun skip(index: Int) {
-        handler.post { player.jumpToItem(index) }
+        handler.post { player.jumpToItem(index, player.isPlaying) }
     }
 
     fun skipToNext() {
