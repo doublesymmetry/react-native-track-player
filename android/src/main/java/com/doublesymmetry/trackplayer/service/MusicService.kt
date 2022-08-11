007d9b48a2382658ee7d4f904ee15f16fa0991dd
Milen Pivchev <milen.pivchev@gmail.com>
2022-08-11 14:18:23 +0200

    Fix removed case

--- android/src/main/java/com/doublesymmetry/trackplayer/service/MusicService.kt
+++ android/src/main/java/com/doublesymmetry/trackplayer/service/MusicService.kt
@@ -424,7 +424,12 @@ class MusicService : HeadlessJsTaskService() {
                             emit(MusicEvents.BUTTON_SET_RATING, this)
                         }
                     }
-                    else -> {}
+                    is MediaSessionCallback.SEEK -> {
+                        Bundle().apply {
+                            putDouble("position", it.position.toDouble())
+                            emit(MusicEvents.BUTTON_SEEK_TO, this)
+                        }
+                    }
                 }
             }
         }
