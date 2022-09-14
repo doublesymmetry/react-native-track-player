7a67ce127c5be00e7dfcdb97e19d4a1386e3b540
Milen Pivchev <milen.pivchev@gmail.com>
2022-09-14 17:00:45 +0200

    Remove semicolon…

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -127,7 +127,7 @@ export interface AndroidOptions {
    * Whether the audio playback notification is also removed when the playback stops. **If `stoppingAppPausesPlayback` is set to false, this will be ignored.**
    */
   stoppingAppRemovesNotification: boolean;
-};
+}
 
 export interface MetadataOptions {
   androidOptions?: AndroidOptions;
