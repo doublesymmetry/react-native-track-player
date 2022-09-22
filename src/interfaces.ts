0da02f25466a02fc2aaf51321dd2096396fcdf7a
Milen Pivchev <milen.pivchev@gmail.com>
2022-09-22 10:19:09 +0200

    Refactor

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -233,17 +233,17 @@ export enum AppKilledPlaybackBehavior {
   /**
    * This option will continue playing audio in the background when the app is removed from recents. The notification remains. This is the default.
    */
-  ContinuePlayback = "continue-playback",
+  ContinuePlayback = 'continue-playback',
 
   /**
    * This option will pause playing audio in the background when the app is removed from recents. The notification remains and can be used to resume playback.
    */
-  PausePlayback = "pause-playback",
+  PausePlayback = 'pause-playback',
 
   /**
    * This option will stop playing audio in the background when the app is removed from recents. The notification is removed and can't be used to resume playback. Users would need to open the app again to start playing audio.
    */
-  StopPlaybackAndRemoveNotification = "stop-playback-and-remove-notification",
+  StopPlaybackAndRemoveNotification = 'stop-playback-and-remove-notification',
 }
 
 export interface AndroidOptions {
