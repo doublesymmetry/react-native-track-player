afebcbb5cec4de7fcb1e8424c0145a95626931ad
Jacob Spizziri <jspizziri@weare5stones.com>
2022-12-19 06:32:22 -0500

    chore(example): update the dev tooling by removing the need for "sync" commands

--- example/android/app/src/main/java/com/example/MainApplication.java
+++ example/android/app/src/main/java/com/example/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.soloader.SoLoader;
 import com.example.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.doublesymmetry.trackplayer.TrackPlayer;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -28,6 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
             List<ReactPackage> packages = new PackageList(this).getPackages();
             // Packages that cannot be autolinked yet can be added manually here, for example:
             // packages.add(new MyReactNativePackage());
+            // manually link our package
+            packages.add(new TrackPlayer());
             return packages;
         }
 
