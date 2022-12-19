4fe331c02a7f1f67de59b225536d26316a95f65e
Jacob Spizziri <jspizziri@weare5stones.com>
2022-12-19 06:32:22 -0500

    chore(example): update the dev tooling by removing the need for "sync" commands

--- example/metro.config.js
+++ example/metro.config.js
@@ -5,18 +5,42 @@
  * @format
  */
 const path = require('path');
+const escape = require('escape-string-regexp');
 const exclusionList = require('metro-config/src/defaults/exclusionList');
+const pak = require('../package.json');
+
+const root = path.resolve(__dirname, '..');
+const modules = Object.keys({
+  ...(pak.peerDependencies),
+});
+
+/** build the blockList **/
+const blockList = modules.map(
+  (m) => new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
+);
+// This stops "react-native run-windows" from causing the metro server to crash if its already running
+blockList.push(new RegExp(
+  `${path.resolve(__dirname, 'windows').replace(/[/\\]/g, '/')}.*`,
+));
+// This prevents "react-native run-windows" from hitting: EBUSY: resource busy or locked, open msbuild.ProjectImports.zip
+blockList.push(/.*\.ProjectImports\.zip/);
+
+/** build extraNodeModules **/
+const extraNodeModules = modules.reduce((acc, name) => {
+  acc[name] = path.join(__dirname, 'node_modules', name);
+  return acc;
+}, {});
 
 module.exports = {
+  projectRoot: __dirname,
+  watchFolders: [root],
+
+  // We need to make sure that only one version is loaded for peerDependencies
+  // So we exclude them at the root, and alias them to the versions in
+  // example's node_modules
   resolver: {
-    blockList: exclusionList([
-      // This stops "react-native run-windows" from causing the metro server to crash if its already running
-      new RegExp(
-        `${path.resolve(__dirname, 'windows').replace(/[/\\]/g, '/')}.*`,
-      ),
-      // This prevents "react-native run-windows" from hitting: EBUSY: resource busy or locked, open msbuild.ProjectImports.zip
-      /.*\.ProjectImports\.zip/,
-    ]),
+    blockList: exclusionList(blockList),
+    extraNodeModules,
   },
   transformer: {
     getTransformOptions: async () => ({
