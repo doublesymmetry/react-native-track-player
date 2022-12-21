a9b0ab23f4a8474ec7e322698abf15e0f9211ef3
Jacob Spizziri <jspizziri@weare5stones.com>
2022-12-21 12:20:11 -0500

    v4.0.0-web.5

--- src/interfaces/PlaybackState.ts
+++ src/interfaces/PlaybackState.ts
@@ -2,5 +2,5 @@ import { State } from '../constants';
 import type { PlaybackErrorEvent } from './events';
 
 export type PlaybackState =
-  | { state: Exclude<State, State.Error>; }
-  | { state: State.Error; error: PlaybackErrorEvent; };
+  | { state: Exclude<State, State.Error> }
+  | { state: State.Error; error: PlaybackErrorEvent };
