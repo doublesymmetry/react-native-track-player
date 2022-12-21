b33cc5b34b02c65a5f21f97fb4f80c11dd3e5919
Jacob Spizziri <jspizziri@weare5stones.com>
2022-12-21 12:14:03 -0500

    fix(interfaces): update the definition of PlaybackState to use Exclude<>

--- src/interfaces/PlaybackState.ts
+++ src/interfaces/PlaybackState.ts
@@ -2,18 +2,5 @@ import { State } from '../constants';
 import type { PlaybackErrorEvent } from './events';
 
 export type PlaybackState =
-  | {
-      state:
-        | State.None
-        | State.Buffering
-        | State.Loading
-        | State.Playing
-        | State.Paused
-        | State.Ready
-        | State.Paused
-        | State.Stopped;
-    }
-  | {
-      state: State.Error;
-      error: PlaybackErrorEvent;
-    };
+  | { state: Exclude<State, State.Error>; }
+  | { state: State.Error; error: PlaybackErrorEvent; };
