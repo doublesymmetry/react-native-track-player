6899f9452d19d836a190a058a9298c52386bcca0
puckey <jonathan@puckey.studio>
2023-03-24 12:41:04 +0100

    chore(android): remove unused imports

--- android/src/main/java/com/doublesymmetry/trackplayer/service/MusicService.kt
+++ android/src/main/java/com/doublesymmetry/trackplayer/service/MusicService.kt
@@ -2,7 +2,6 @@ package com.doublesymmetry.trackplayer.service
 
 import android.annotation.TargetApi
 import android.app.*
-import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Binder
@@ -11,10 +10,6 @@ import android.os.Bundle
 import android.os.IBinder
 import android.support.v4.media.RatingCompat
 import androidx.annotation.MainThread
-import androidx.annotation.NonNull
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationCompat.PRIORITY_LOW
-import androidx.lifecycle.ProcessLifecycleOwner
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.doublesymmetry.kotlinaudio.models.*
 import com.doublesymmetry.kotlinaudio.models.NotificationButton.*
@@ -28,7 +23,6 @@ import com.doublesymmetry.trackplayer.model.Track
 import com.doublesymmetry.trackplayer.model.TrackAudioItem
 import com.doublesymmetry.trackplayer.module.MusicEvents
 import com.doublesymmetry.trackplayer.module.MusicEvents.Companion.EVENT_INTENT
-import com.doublesymmetry.trackplayer.utils.AppForegroundTracker
 import com.doublesymmetry.trackplayer.utils.BundleUtils
 import com.doublesymmetry.trackplayer.utils.BundleUtils.setRating
 import com.facebook.react.HeadlessJsTaskService
