430bc66cf46248fd6cc3f8048628ee2dc5feaf65
unknown <106490582+lovegaoshi@users.noreply.github.com>
2023-08-25 21:01:29 -0700

    fix: linting

--- src/constants/MediaItemPlayable.ts
+++ src/constants/MediaItemPlayable.ts
@@ -1,6 +1,3 @@
-import { NativeModules } from 'react-native';
-const { TrackPlayerModule: TrackPlayer } = NativeModules;
-
 export enum MediaItemPlayable {
   MediaPlayable = '0',
   MediaBrowsable = '1',
