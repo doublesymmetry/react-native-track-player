55248f3feeb6db8d104bd6a7716ffc480d290991
Jacob Spizziri <jspizziri@weare5stones.com>
2023-09-18 09:51:06 -0400

    chore(example): update the web build to work with the upgraded example code

--- example/craco.config.js
+++ example/craco.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 const {CracoAliasPlugin} = require('react-app-alias-ex');
 const babelInclude = require('@dealmore/craco-plugin-babel-include');
 const webpack = require('webpack');
+const { loaderByName, addBeforeLoader } = require('@craco/craco');
+
 
 module.exports = {
   webpack: {
@@ -12,19 +14,39 @@ module.exports = {
       'react': path.resolve(__dirname, './node_modules/react'),
     },
     plugins: {
-        add: [
-          new webpack.EnvironmentPlugin({ JEST_WORKER_ID: null }),
-          new webpack.DefinePlugin({
-            process: { env: {} },
-            __DEV__: true,
-          })
-        ],
-      },
+      add: [
+        new webpack.EnvironmentPlugin({ JEST_WORKER_ID: null }),
+        new webpack.DefinePlugin({
+          process: { env: {} },
+          __DEV__: true,
+        })
+      ],
+    },
+    configure: (config) => {
+      // BEGIN: resolve react-native-vector-icons font files
+      config.resolve.extensions.push('.ttf');
+      addBeforeLoader(
+        config,
+        loaderByName('url-loader'),
+        {
+          test: /\.ttf$/,
+          loader: 'url-loader', // or directly file-loader
+          include: path.resolve(__dirname, 'node_modules/react-native-vector-icons'),
+        },
+      );
+      // END: resolve react-native-vector-icons font files
+
+      return config;
+    },
   },
   babel: {
     presets: [
       '@babel/preset-react',
       '@babel/preset-typescript',
+    ],
+    plugins: [
+      '@babel/plugin-proposal-export-namespace-from',
+      'react-native-reanimated/plugin',
     ],
   },
   plugins: [
