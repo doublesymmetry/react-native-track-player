cb53154c811e2f3ce7a4d0160e57abab1a7947ef
kirillzyusko <zyusko.kirik@gmail.com>
2023-10-17 14:50:19 +0200

    refactor: `usePlaybackState` without initial `undefined`

--- src/hooks/usePlaybackState.ts
+++ src/hooks/usePlaybackState.ts
@@ -1,42 +1,34 @@
 import { useEffect, useState } from 'react';
 
-import { getPlaybackState, addEventListener } from '../trackPlayer';
-import { Event } from '../constants';
+import { addEventListener } from '../trackPlayer';
+import { Event, State } from '../constants';
 import type { PlaybackState } from '../interfaces';
+
+class PlaybackStateSingleton {
+  public state = State.None;
+
+  constructor() {
+    addEventListener(Event.PlaybackState, e => {
+      this.state = e.state;
+    });
+  }
+}
+
+const PlaybackStateProvider = new PlaybackStateSingleton();
 
 /**
  * Get current playback state and subsequent updates.
- *
- * Note: While it is fetching the initial state from the native module, the
- * returned state property will be `undefined`.
  * */
-export const usePlaybackState = (): PlaybackState | { state: undefined } => {
-  const [playbackState, setPlaybackState] = useState<
-    PlaybackState | { state: undefined }
-  >({
-    state: undefined,
-  });
+export const usePlaybackState = (): PlaybackState => {
+  const [playbackState, setPlaybackState] = useState<PlaybackState>({
+    state: PlaybackStateProvider.state,
+  } as PlaybackState);
   useEffect(() => {
-    let mounted = true;
-
-    getPlaybackState()
-      .then((fetchedState) => {
-        if (!mounted) return;
-        // Only set the state if it wasn't already set by the Event.PlaybackState listener below:
-        setPlaybackState((currentState) =>
-          currentState.state ? currentState : fetchedState
-        );
-      })
-      .catch(() => {
-        /** getState only throw while you haven't yet setup, ignore failure. */
-      });
-
-    const sub = addEventListener(Event.PlaybackState, (state) => {
+    const sub = addEventListener(Event.PlaybackState, state => {
       setPlaybackState(state);
     });
 
     return () => {
-      mounted = false;
       sub.remove();
     };
   }, []);
