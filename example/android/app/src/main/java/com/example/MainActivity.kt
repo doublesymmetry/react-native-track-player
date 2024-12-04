43a685d50bb8d3e3db25c7532c583a677856ddf6
puckey <jonathan@puckey.studio>
2024-12-04 14:12:18 +0100

    android: replace Log with Timber for logging

--- example/android/app/src/main/java/com/example/MainActivity.kt
+++ example/android/app/src/main/java/com/example/MainActivity.kt
@@ -5,12 +5,12 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
-import android.util.Log
+import timber.log.Timber
 
 class MainActivity : ReactActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d("APM", "attempt to start activity")
+        Timber.d("attempt to start activity")
         super.onCreate(savedInstanceState)
     }
 
