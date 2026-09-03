6dbbb8bdd346a96940ea031df9ba8808d38a2b5c
Brian Lenz <brian@throughtheword.org>
2026-09-03 12:26:12 -0600

    Reproduce Broken iOS Control Center Issue w/ react-native-video
    * Updated `@rntp/player` to the latest `5.9.2` release.
    * Added `react-native-video`'s latest beta 11 dependency (and the `react-native-nitro-modules` peer dependency).
    * Added a TestVideo component to exhibit the issue where react-native-video can "break" RNTP.
    * Updated the MusicScreen to include the TestVideo component by default. There is a button to remove (unmount) the video to allow further testing of RNTP after react-native-video is out of the picture.
    * Updated the playback config on the MusicScreen to use skip forward/backward instead of next/previous. This helps make the iOS Control Center Now Playing issue more prominent/obvious.
    * Updated index.js to call `setAudioSessionManagementDisabled(true)` to (try to) ensure that `react-native-video` doesn't interfere with RNTP's Now Playing behavior.
    * Updated the example project to use the latest version of Yarn.
    * Updated the `Podfile` to fix a build issue with `glog`.
    * Set the Ruby version to `3.3.0` via `.ruby-version`.
    * Removed the Carplay audio entitlement since it isn't needed for the repro and was causing device deployment issues.
    
    https://github.com/doublesymmetry/react-native-track-player/issues/2679

--- example/index.js
+++ example/index.js
@@ -3,6 +3,9 @@ import TrackPlayer from '@rntp/player';
 import App from './src/App';
 import { TaskService } from './src/services/TaskService';
 import { name as appName } from './app.json';
+import { setAudioSessionManagementDisabled } from "react-native-video";
+
+setAudioSessionManagementDisabled(true);
 
 TrackPlayer.registerBackgroundEventHandler(() => TaskService);
 AppRegistry.registerComponent(appName, () => App);
